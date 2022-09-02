aae53fd8dc66d54796fec2e88754d50318ed5d36
Gabriel Lins <gabriellf.glf@gmail.com>
2022-09-02 02:52:27 -0300

    unfinished
    projeto em C incompleto.

---
+++ testes-c/teste.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <string.h>
+
+
+typedef struct 
+{
+  int codigo;
+  int preco;
+  int quantidade;
+  char nome[30];
+}Mercadoria;
+
+Mercadoria estoque[];
+
+Mercadoria Cadastrar(){
+    Mercadoria produto;
+
+    printf("Qual codigo da mercadoria? :");
+    scanf("%d", &produto.codigo);
+    fflush(stdin);
+    printf("Qual o valor da mercadoria? :");
+    scanf("%d", &produto.preco);
+    fflush(stdin);
+    printf("Qual a quantidade? : ");
+    scanf("%d", &produto.quantidade);
+    fflush(stdin);
+    printf("Qual o nome da mercadoria? :");
+    fgets(produto.nome, 29, stdin);
+    fflush(stdin);
+    printf("\n");
+
+    return produto;
+}
+
+int main()
+{ 
+  int opcao;
+  int i = 0;
+
+  for (;;)
+  {
+
+    printf("1 - Registrar mercadorias\n2 - Consultar mercadorias\n3 - Valor total da mercadoria\n4 - Sair\n");
+    printf("\nDigite a opcao desejada:");
+    scanf("%d", &opcao);
+    
+    int i = 0;
+    int qtd_cadastro;
+
+    if (opcao == 1)
+    {
+      
+     
+
+      printf("quantidade de cadastros: ");
+      scanf("%d", &qtd_cadastro);
+
+      for (i = 0; i < qtd_cadastro; i++){
+        estoque[i] = Cadastrar();
+        printf("index da mercadoria: %d\n", i);
+      }
+    }
+
+    //save = i;
+    
+    
+    if (opcao == 2)
+    {
+      int index = 0;
+      for (int i = 0; i < qtd_cadastro; i++)
+      {
+        printf("Nome: %s", estoque[i].nome);
+        printf("Index: %d\n", index);
+        index++;
+      }
+      
+      
+      printf("index da mercadoria desejada: ");
+      scanf("%d", &i);
+      system("cls");
+      printf("NOME: %s", estoque[i].nome);
+      printf("\n");
+      printf("Codigo do produto: %d", estoque[i].codigo);
+      printf("\n");
+      printf("Quantidade: %d", estoque[i].quantidade);
+      printf("\n");
+      printf("Preco do produto: %dR$\n", estoque[i].preco);
+      printf("\n");
+    }
+    
+    if(opcao == 4){
+      printf("Voce saiu!");
+    break;
+    }
+    //printf("i: %d\n", i);
+    //printf("---%d---\n", save);
+  }
+
+      
+      
+      
+    /*case 2:
+      printf("Qual o codigo da mercadoria a ser consultada?: ");
+      scanf("%d");
+      break;
+    case 3:
+      system("cls");
+      printf("O valor do total de mercadorias é: %f");
+      break;
+
+    case 4:
+      system("cls");
+      printf("\nVoce saiu!\n\n");
+      return;
+      break;
+    }
+  }*/
+
+  return 0;
+}
